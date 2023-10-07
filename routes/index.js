14c9a2f7d1de244028d8cdc4c3cca25a4b5009b5
Ian Kamar <ian.kamar@outlook.com>
2023-10-07 18:04:32 -0400

    update my project

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const users = require("./users");
 // eslint-disable-next-line camelcase
-const clothingItem = require("./clothingItem");
+const clothingItem = require("./clothingitem");
 const { ERROR_CODE_400 } = require("../utils/errors");
 
 router.use("/users", users);
