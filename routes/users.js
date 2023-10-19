d4ed86f1d6ed574a14e6aa5d8dedcd19cabfbed1
Ian Kamar <ian.kamar@outlook.com>
2023-10-19 05:29:29 -0400

    update my project

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,6 @@ const { getCurrentUser, updateUser } = require("../controllers/users");
 const auth = require("../middlewares/auth");
 
 router.get("/me", auth, getCurrentUser);
-router.get("/me", auth, updateUser);
+router.patch("/me", auth, updateUser);
 
 module.exports = router;
