7db96cc3e1823c1fdbec1f6b2b18534d6642f2ce
Ian Kamar <ian.kamar@outlook.com>
2023-11-21 03:40:42 -0500

    update my project

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,4 +1,3 @@
-/*
 const { InternalServerError } = require("../utils/errors");
 
 const errorHandler = (err, req, res, next) => {
@@ -6,28 +5,6 @@ const errorHandler = (err, req, res, next) => {
   // const errorStatus = err.statusCode || InternalServerError;
   const errorStatus = err.statusCode || InternalServerError.statusCode || 500;
   const errorMessage = err.message || "Something went wrong";
-
-  res.status(errorStatus).json({
-    success: false,
-    status: errorStatus,
-    message: errorMessage,
-    stack: process.env.NODE_ENV === "development" ? err.stack : {},
-  });
-};
-
-module.exports = errorHandler;
-*/
-const { InternalServerError } = require("../utils/errors");
-
-const errorHandler = (err, req, res, next) => {
-  console.log("Middleware Error Handling");
-  console.log("Error object:", err);
-
-  const errorStatus = err.statusCode || InternalServerError.statusCode;
-  console.log("Resolved status code:", errorStatus);
-
-  const errorMessage = err.message || "Something went wrong";
-  console.log("Resolved error message:", errorMessage);
 
   res.status(errorStatus).json({
     success: false,
